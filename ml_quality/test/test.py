cbfe613940770fa4888d515b3e5b8f85d45999a3
Marina Belyanova <flerchy@gmail.com>
2017-05-10 14:56:40 +0300

    added codestyle checking for file

--- ml_quality/test/test.py
+++ ml_quality/test/test.py
@@ -3,6 +3,8 @@ import subprocess
 import numpy as np
 from sklearn.externals import joblib
 import pythonparse 
+import re
+import json
 bad_array = np.load('../train/bad.all.npy')
 good_array = np.load('../train/good.all.npy')
 
@@ -11,16 +13,32 @@ clf = joblib.load('../model.pkl')
 
 parser = argparse.ArgumentParser(description="Test file on this path")
 parser.add_argument('filename', metavar='f', help="Path to file")
+parser.add_argument('cscheck', metavar='c', help="Set if you want to see codestyle checker output")
 
 args = parser.parse_args()
 
-print args
+print args.cscheck
 
+if (args.cscheck == "1"):
+    
+    subprocess.check_call('touch cscheck.out', shell=True)
+    cscheck = subprocess.check_output('pycodestyle ' + str(args.filename) + ' | cat > cscheck.out', shell=True)
+    cs_file = open("cscheck.out")
+    num_lines = []
+    err = []
+    for line in cs_file:
+        num_lines.append(re.search(r'\d+', line).group())
+        err.append(line.rsplit(':', 1)[-1][:-1])
+    cs_res = open("cscheck.json", 'w+')
+    res_dict = dict(zip(num_lines, err))
+    cs_res.write(json.dumps(res_dict))
+    cs_res.close()
+    
 pythonparse.run(args.filename)
 
 
 subprocess.check_call('touch lint.out', shell=True)
-subprocess.check_call('pylint ' + str(args.filename) + ' | wc -l > lint.out', shell=True)
+subprocess.check_call('pycodestyle ' + str(args.filename) + ' | wc -l > lint.out', shell=True)
 file = open("res.out")
 
 X_test = []
