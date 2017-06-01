d94164a0be8b2edc4c5959c24eb84edb7c927cd8
Marina Belyanova <flerchy@gmail.com>
2017-06-02 00:43:02 +0300

    added average metrics

--- alike/feature_extraction.py
+++ alike/feature_extraction.py
@@ -2,6 +2,7 @@
 
 import pycodestyle.pycodestyle as cs
 import pyparser.parser as ps
+import json
 
 import os
 
@@ -15,14 +16,28 @@ def main():
             if f.endswith(".py"):
                 #print f
                 fi = open(os.path.join(root, f), 'r')
-                one_res.append(ps.run(fi))
+                one_res.extend(ps.run(fi))
                 fi.close()
                 fchecker = cs.Checker(os.path.join(root, f), show_source=True)
                 file_errors = fchecker.check_all()
+                #print file_errors
                 one_res.append({"codestyle_errors": file_errors})
                 fi.close()
                 res.append(one_res)
-    print res
+    print json.dumps(res, sort_keys=True, indent=4)
+    avg = {}
+    for entry in res:
+        for metric in entry:
+            for key in metric:
+                avg[key] = 0
+    for entry in res:
+        for metric in entry:
+            for key in metric:
+                avg[key] += int(metric[key])
+    for key in avg:
+        avg[key] *= 1.0/len(entry)
+    print avg
+
 
 
 if __name__ == '__main__':
